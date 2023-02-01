629b781966941d48cb7ad3e66db9a8b469827002
GabrielMalta <gabrielmalta@tecnico.ulisboa.pt>
2023-02-01 02:22:52 +0000

    webpage

---
+++ gpx2javascript.py
@@ -0,0 +1,32 @@
+import gpxpy
+import click
+import os
+
+######################################### 
+# MAIN
+input_folder = "gpx/"
+output_path = "webpage/getPoints.js"
+
+with open(output_path, 'w') as output:
+    print ("\nProcessing files...")
+    output.write("function getPoints() {\n\treturn [\n")
+    with click.progressbar(os.listdir(input_folder)) as bar:
+        for filename in bar:
+            if (filename.lower().endswith(".gpx")):
+                #Verify file is a gpx file
+                gpx_file = open(os.path.join(input_folder, filename))
+                gpx = gpxpy.parse(gpx_file)
+                for track in gpx.tracks:
+                    for segment in track.segments:
+                        for point in segment.points:
+                            output.write("\t\tnew google.maps.LatLng("+
+                                         str(float(point.latitude))+
+                                         ","+
+                                         str(float(point.longitude))+
+                                         "),\n")
+    output.write("\t];\n}")
+
+
+
+
+
